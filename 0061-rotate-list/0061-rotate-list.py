7d908550e184dd3958fee066b26d2efcd880df80
Victor Da Camino <joaodeppermann@gmail.com>
2022-11-27 20:56:28 +0100

    Time: 82 ms (20.78%), Space: 13.9 MB (31.83%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -5,36 +5,33 @@
 #         self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # edge case
         if head is None:
             return None
         
-        # find last node 
-        cur = head
+        # get the size of the list and keep a pointer to the last node 
+        last = head
         size = 1
-        while cur.next:
-            cur = cur.next
+        while last.next:
+            last = last.next
             size += 1
             
-        # find right place to undo connection 
-        last_element = k % size
-        # small optimization
-        if last_element == 0:
-            return head
-        
-        
-        
+        # find which node should be the last one
+        last_element = k % size     
         fast, slow = head, head
         while last_element:
             fast = fast.next
             last_element -= 1
-        
         while fast.next:
             fast = fast.next
             slow = slow.next
-            
-        # cur now has the last element
-        cur.next = head
         
+        # slow is the future last node of the list
+        # last is now rewired to the head (making a temporary cyclic list)
+        last.next = head
+        
+        # change the head to the first node after the last node
         head = slow.next
+        # disconnect last node's next pointer
         slow.next = None
         return head
