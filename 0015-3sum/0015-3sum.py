529e8593615ae8e85cbb70e17f518560ddceb35a
Victor Da Camino <joaodeppermann@gmail.com>
2022-12-12 16:03:06 +0100

    Time: 2248 ms (50.93%), Space: 18.5 MB (15.13%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,14 +1,15 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = set()
-        for i in range(len(nums) - 2):
-            myhash = set()
-            targetSum = -1*nums[i]
-            for j in range(i + 1, len(nums)):
-                if nums[j] in myhash:
-                    res.add(tuple(sorted([nums[j], nums[i], targetSum - nums[j]])))
-                else:
-                    myhash.add(targetSum - nums[j])
+        res, dups = set(), set()
+        seen = {}
+        for i, val1 in enumerate(nums):
+            if val1 not in dups:
+                dups.add(val1)
+                for j, val2 in enumerate(nums[i+1:]):
+                    complement = -val1 - val2
+                    if complement in seen and seen[complement] == i:
+                        res.add(tuple(sorted((val1, val2, complement))))
+                    seen[val2] = i
         return res
         
         
