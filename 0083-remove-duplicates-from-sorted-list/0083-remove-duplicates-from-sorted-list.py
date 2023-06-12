80e39ba31df344b8392a8727836fb50d5ae086dc
Victor Da Camino <joaodeppermann@gmail.com>
2023-06-12 22:42:43 +0200

    Time: 59 ms (35.70%), Space: 16.6 MB (8.69%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -5,25 +5,9 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # edge cases
-        if head is None:
-            return None
-        if head.next is None:
-            return head
-        # keep dummy head just to return the orignal head
-        dummy_head = ListNode(val=0, next=head)
-        # keep track of current good node 
-        first, second = head, head.next
-        while second is not None:
-            # check if you need to get second out or not 
-            if second.val == first.val:
-                # need to pop the second Node
-                first.next = second.next
-            else:
-                # no need to pop, just keep going 
-                first = first.next
-            # second pointer always move 
-            second = second.next
-        return dummy_head.next
+        if head and head.next:
+            head.next = self.deleteDuplicates(head.next)
+            return head.next if head.next.val == head.val else head
+        return head
         
         
