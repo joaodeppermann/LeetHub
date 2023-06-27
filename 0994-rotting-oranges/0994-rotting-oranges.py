1229bf0783cbf07c71931205d4781cb1257b447f
Victor Da Camino <joaodeppermann@gmail.com>
2023-06-27 16:52:42 +0200

    Time: 73 ms (30.50%), Space: 16.3 MB (92.97%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,27 +1,45 @@
+from collections import deque
+
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        visited = set() # hash set containing explored coordinates
+        freshes = 0 # number of fresh oranges
+        res = 0 # minutes it took to rotten all the oranges
         ROWS, COLS = len(grid), len(grid[0])
-        directions = [[1, 0], [-1, 0], [0, 1], [0 , -1]]
-        q = collections.deque()
-        freshes_remaining, minutes_passed = 0, 0
+        DIRECTIONS = [(0,1), (-1,0), (0,-1), (1,0)]
+        # edge case where we are given an empty grid 
+        if ROWS == 0 or COLS == 0:
+            return -1
+        queue = deque()
+        # traverse the grid once and add to the BFS queue the intial rotten oranges
+        for row in range(0, ROWS):
+            for col in range(0, COLS):
+                # add intial rotten orange to the queue 
+                if grid[row][col] == 2:
+                    queue.append((row, col))
+                elif grid[row][col] == 1:
+                    freshes += 1
+                
+        if freshes == 0:
+            return 0
         
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == 1:
-                    freshes_remaining += 1
-                elif grid[r][c] == 2:
-                    q.append((r, c))
+        # BFS through the grid starting by the queue mentioned previously 
+        while queue:
+            oranges_in_this_level = len(queue)
+            while oranges_in_this_level != 0:
+                cur_row, cur_col = queue.popleft()
+                # visit 4 directions
+                for delta_row, delta_col in DIRECTIONS:
+                    new_row, new_col = cur_row + delta_row, cur_col + delta_col
+                    # try to explore neighbour 
+                    if (new_row in range(0, ROWS) and 
+                        new_col in range(0, COLS) and 
+                        (new_row, new_col) not in visited and
+                        grid[new_row][new_col] == 1):
+                        freshes -= 1
+                        queue.append((new_row, new_col))
+                        visited.add((new_row, new_col))
+                oranges_in_this_level -= 1
+            res += 1
             
-        while q and freshes_remaining:
-            number_oranges_in_this_level = len(q)  # get the number of simultaneous bfs we need to perform
-            for _ in range(number_oranges_in_this_level):
-                cur_row, cur_col = q.popleft()
-                for row_offset, col_offset in directions:
-                    new_row, new_col = cur_row + row_offset, cur_col + col_offset
-                    if new_row in range(ROWS) and new_col in range(COLS) and grid[new_row][new_col] == 1:
-                        freshes_remaining -= 1
-                        grid[new_row][new_col] = 2
-                        q.append((new_row, new_col))
-            minutes_passed += 1
-            
-        return minutes_passed if freshes_remaining == 0 else -1
+        return res - 1 if freshes == 0 else -1
