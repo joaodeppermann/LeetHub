bfa3888a255373877c7b0519cfdd5776e461b159
Victor Da Camino <joaodeppermann@gmail.com>
2023-06-29 23:22:54 +0200

    Time: 473 ms (68.05%), Space: 18.7 MB (55.89%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,24 +1,23 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        adj_list = {i:list() for i in range(1, n + 1)} # each graph with a list of its neighbors (weight_to_neighbor, neighbor)
-        for source, target, weight in times:
-            adj_list[source].append((weight, target))
-        visited = set()
-        min_heap = [(0, k)] # current_min_weight, current node 
+        adj_list = {i:[] for i in range(1, n + 1)}
+        for start, end, weight in times:
+            adj_list[start].append((weight, end))
+
+        distances = [+inf]*(n + 1)
+        distances[0], distances[k] = 0, 0
+        
+        min_heap = [(0, k)]
         heapq.heapify(min_heap)
-        res = 0
         
         while min_heap:
             cur_weight, cur_node = heapq.heappop(min_heap)
-            if cur_node in visited:
-                continue # this is not the best path
-            visited.add(cur_node)
-            res = max(res, cur_weight)
-            # explore all the neighbors of the current node, adding possible weights to min_heap
-            for neighbor_weight, neighbor_node in adj_list[cur_node]:
-                new_weight = cur_weight + neighbor_weight
-                heapq.heappush(min_heap, (new_weight, neighbor_node))
-                
-        return res if len(visited) == n else -1
+            
+            for next_weight, next_node in adj_list[cur_node]:
+                if cur_weight + next_weight < distances[next_node]:
+                    distances[next_node] = cur_weight + next_weight
+                    heapq.heappush(min_heap, (distances[next_node], next_node))
+                    
+        return max(distances) if max(distances) != +inf else -1
                 
                 
