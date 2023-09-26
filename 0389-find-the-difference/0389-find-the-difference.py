e2c3fbdb1ec5ee15e204df69de481f06ff0dfbd8
João Deppermann <joaodeppermann@gmail.com>
2023-09-26 15:50:28 +0200

    Time: 39 ms (71.21%), Space: 16.2 MB (95.97%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,8 +1,13 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        counter_s = Counter(s)
-        counter_t = Counter(t)
+        # counter_s = Counter(s)
+        # counter_t = Counter(t)
+        # for c in t:
+        #     if counter_t[c] > counter_s[c]:
+        #         return c
+        sum_t = sum_s = 0
         for c in t:
-            if counter_t[c] > counter_s[c]:
-                return c
-            
+            sum_t += ord(c)
+        for c in s:
+            sum_s += ord(c)
+        return chr(sum_t - sum_s)
