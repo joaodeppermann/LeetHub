38f4492dc49713259e344f0e5f82a0909a90c867
João Deppermann <joaodeppermann@gmail.com>
2023-09-26 15:30:10 +0200

    Time: 69 ms (94.03%), Space: 16 MB (99.24%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.py
+++ 0799-champagne-tower/0799-champagne-tower.py
@@ -6,12 +6,15 @@ class Solution:
         1
         1
         # Pattern: each node will have as children the following row index i and i + 1
-        tower = [[0]*k for k in range(1, query_row + 3)]
-        tower[0][0] = poured
-        for i in range(query_row + 1):
-            for j in range(i + 1):
-                extra = tower[i][j] - 1
+        prev_row = [poured]
+        for row in range(1, query_row + 1):
+            # Create child row
+            cur_row = [0]*(row + 1)
+            # Traverse parent row
+            for j in range(row):
+                extra = prev_row[j] - 1
                 if extra > 0:
-                    tower[i + 1][j] += extra / 2
-                    tower[i + 1][j + 1] += extra / 2
-        return min(1, tower[query_row][query_glass])
+                    cur_row[j] += extra / 2
+                    cur_row[j + 1] += extra / 2
+            prev_row = cur_row
+        return min(1, prev_row[query_glass])
