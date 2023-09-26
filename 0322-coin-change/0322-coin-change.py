7b6c1fc82ff71520692f2df4fd3b9ec043da2936
João Deppermann <joaodeppermann@gmail.com>
2023-09-26 23:41:16 +0200

    Time: 1156 ms (26.65%), Space: 46.5 MB (8.26%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,11 +1,32 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        # Edge case
         if amount == 0:
             return 0
-        dp = [+inf]*(amount+1)
-        dp[0] = 0
-        for a in range(amount + 1):
+        memo = {}
+        def dfs(remainder):
+            # Check if we have already solved this problem
+            if remainder in memo:
+                return memo[remainder]
+            # Base cases
+            # 1. Solution not found in this path
+            if remainder < 0:
+                return -1
+            # 2. Solution found in this path
+            if remainder == 0:
+                return 0
+            
+            # Try to use all the coins
+            min_coins = +inf
             for coin in coins:
-                if a - coin >= 0:
-                    dp[a] = min(dp[a], 1 + dp[a - coin])
-        return dp[-1] if dp[-1] != +inf else -1
+                res = dfs(remainder - coin)
+                if res != -1:
+                    # This path lead to a solution, check if it's optimal
+                    min_coins = min(min_coins, res + 1)
+            
+            # Cache the result before returning
+            memo[remainder] = min_coins
+            # Return best solution we have found for this subproblem
+            return min_coins
+        dfs(amount)
+        return memo[amount] if memo[amount] != +inf else -1
