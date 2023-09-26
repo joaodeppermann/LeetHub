e0d459f492287b85772af7c071b747b3ddebe629
João Deppermann <joaodeppermann@gmail.com>
2023-09-26 19:41:02 +0200

    Time: 1005 ms (45.30%), Space: 16.6 MB (89.98%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,26 +1,11 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-    
-        memo = {}
-        def dfs(curSum):
-            # Check cache
-            if curSum in memo:
-                return memo[curSum]
-        
-            # Base cases
-            if curSum > amount:
-                return -1
-            if curSum == amount:
-                return 0
-        
-            # Analyse all the paths and get minimum cost
-            min_cost = +inf
+        if amount == 0:
+            return 0
+        dp = [+inf]*(amount+1)
+        dp[0] = 0
+        for a in range(amount + 1):
             for coin in coins:
-                res = dfs(curSum + coin)
-                if res != -1:
-                    min_cost = min(min_cost, res + 1)
-            
-            # Cache the return value of the dfs function
-            memo[curSum] = min_cost
-            return memo[curSum] if min_cost != +inf else -1
-        return dfs(0)
+                if a - coin >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - coin])
+        return dp[-1] if dp[-1] != +inf else -1
