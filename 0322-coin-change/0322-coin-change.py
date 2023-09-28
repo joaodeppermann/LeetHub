d7f7ddb74102d17886c60d751c5a5c1e13e80eb0
João Deppermann <joaodeppermann@gmail.com>
2023-09-28 20:50:00 +0200

    Time: 1339 ms (15.15%), Space: 34.8 MB (17.21%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,10 +1,35 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [+inf]*(amount + 1)
-        dp[0] = 0
-        for a in range(1, amount + 1):
+        if amount == 0:
+            return 0
+        cache = {}
+        path = defaultdict(list)
+        def dfs(remainder):
+            if remainder in cache:
+                return cache[remainder]
+            # base cases
+            if remainder == 0:
+                return (0, [])
+            if remainder < 0:
+                return (-1, [])
+            
+            min_coins = +inf
             for coin in coins:
-                # Check if you can use the coin
-                if coin <= a:
-                    dp[a] = min(dp[a], 1 + dp[a - coin])
-        return dp[amount] if dp[amount] != +inf else -1
+                ret = dfs(remainder - coin)
+                ret_num = ret[0]
+                ret_path = ret[1]
+                if ret_num != -1:
+                    if min_coins > ret_num + 1:
+                        min_coins = ret_num + 1
+                        # Update path using this coins
+                        path[remainder] = ret_path.append(coin)
+                        
+            if not path[remainder]:
+                path[remainder] = []
+                   
+            cache[remainder] = (min_coins, path[remainder])
+            return cache[remainder]
+        
+        dfs(amount)
+        return cache[amount][0] if cache[amount][0] != +inf else -1
+                
