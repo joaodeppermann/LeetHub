68b11b50318f747a0ae173e5c19b506c19e1ee35
João Deppermann <joaodeppermann@gmail.com>
2023-09-28 20:12:21 +0200

    Time: 910 ms (78.77%), Space: 16.7 MB (71.09%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,26 +1,10 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # Edge case
-        if amount == 0:
-            return 0
-        cache = {}
-        def dfs(remainder):
-            # Check cache to avoid repeated work
-            if remainder in cache:
-                return cache[remainder]
-            # Base cases
-            if remainder == 0:
-                return 0
-            if remainder < 0:
-                return -1
-            
-            min_coins = +inf
+        dp = [+inf]*(amount + 1)
+        dp[0] = 0
+        for a in range(1, amount + 1):
             for coin in coins:
-                ret = dfs(remainder - coin)
-                if ret != -1:
-                    min_coins = min(min_coins, ret + 1)
-                    
-            cache[remainder] = min_coins
-            return cache[remainder]
-        dfs(amount)
-        return cache[amount] if cache[amount] != +inf else -1
+                # Check if you can use the coin
+                if coin <= a:
+                    dp[a] = min(dp[a], 1 + dp[a - coin])
+        return dp[amount] if dp[amount] != +inf else -1
